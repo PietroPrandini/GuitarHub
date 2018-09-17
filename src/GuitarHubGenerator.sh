f327b8b49b7830235c0827f5136e8502bf365481
buffyeah <prandinip@gmail.com>
2018-09-17 12:22:00 +0200

    Brochure Alpha

--- src/GuitarHubGenerator.sh
+++ src/GuitarHubGenerator.sh
@@ -3,14 +3,34 @@
 echo '
 --> Generating GuitarHubAlphabeticNoteNames.pdf <--'
 arara GuitarHubAlphabeticNoteNames.tex
-mv GuitarHubAlphabeticNoteNames.pdf ../GuitarHubAlphabeticNoteNames.pdf
 
 echo '
 --> Generating GuitarHubLyrics.pdf <--'
 arara GuitarHubLyrics.tex
-mv GuitarHubLyrics.pdf ../GuitarHubLyrics.pdf
 
 echo '
 --> Generating GuitarHubSolfegeNoteNames.pdf <--'
 arara GuitarHubSolfegeNoteNames.tex
+
+echo '
+--> Generating GuitarHubAlphabeticNoteNamesBrochure.pdf'
+arara GuitarHubAlphabeticNoteNamesBrochure.tex
+
+echo '
+--> Generating GuitarHubLyricsBrochure.pdf'
+arara GuitarHubLyricsBrochure.tex
+
+echo '
+--> Generating GuitarHubSolfegeNoteNamesBrochure.pdf'
+arara GuitarHubSolfegeNoteNamesBrochure.tex
+
+#	Moving the files generated
+mv GuitarHubAlphabeticNoteNames.pdf ../GuitarHubAlphabeticNoteNames.pdf
 mv GuitarHubSolfegeNoteNames.pdf ../GuitarHubSolfegeNoteNames.pdf
+mv GuitarHubLyrics.pdf ../GuitarHubLyrics.pdf
+mv GuitarHubAlphabeticNoteNamesBrochure.pdf ../GuitarHubAlphabeticNoteNamesBrochure.pdf
+mv GuitarHubLyricsBrochure.pdf ../GuitarHubLyricsBrochure.pdf
+mv GuitarHubSolfegeNoteNamesBrochure.pdf ../GuitarHubSolfegeNoteNamesBrochure.pdf
+echo '
+--> pdf generated moved to the main directory of GuitarHub
+'
