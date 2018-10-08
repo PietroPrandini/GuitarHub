beb3aeca321f5a1f0c2910df48949055e49b397d
buffyeah <prandinip@gmail.com>
2018-10-08 10:28:50 +0200

    GuitarHubLyricsCompressedVersion + GuitarHubGenerator.sh update

--- src/GuitarHubGenerator.sh
+++ src/GuitarHubGenerator.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
 
 echo '
---> Generating GuitarHubAlphabeticNoteNames.pdf <--'
-arara GuitarHubAlphabeticNoteNames.tex
+--> Generating the booklets <--'
+arara GuitarHub.tex
 
-echo '
---> Generating GuitarHubLyrics.pdf <--'
-arara GuitarHubLyrics.tex
+#echo '
+#--> Generating GuitarHubAlphabeticNoteNames.pdf <--'
+#arara GuitarHubAlphabeticNoteNames.tex
 
-echo '
---> Generating GuitarHubSolfegeNoteNames.pdf <--'
-arara GuitarHubSolfegeNoteNames.tex
+#echo '
+#--> Generating GuitarHubLyrics.pdf <--'
+#arara GuitarHubLyrics.tex
 
-echo '
---> Generating GuitarHubAlphabeticNoteNamesBooklet.pdf'
-arara GuitarHubAlphabeticNoteNamesBooklet.tex
+#echo '
+#--> Generating GuitarHubSolfegeNoteNames.pdf <--'
+#arara GuitarHubSolfegeNoteNames.tex
 
-echo '
---> Generating GuitarHubLyricsBooklet.pdf'
-arara GuitarHubLyricsBooklet.tex
+#echo '
+#--> Generating GuitarHubAlphabeticNoteNamesBooklet.pdf'
+#arara GuitarHubAlphabeticNoteNamesBooklet.tex
 
-echo '
---> Generating GuitarHubSolfegeNoteNamesBooklet.pdf'
-arara GuitarHubSolfegeNoteNamesBooklet.tex
+#echo '
+#--> Generating GuitarHubLyricsBooklet.pdf'
+#arara GuitarHubLyricsBooklet.tex
+
+#echo '
+#--> Generating GuitarHubSolfegeNoteNamesBooklet.pdf'
+#arara GuitarHubSolfegeNoteNamesBooklet.tex
 
 #	Moving the files generated
 mv GuitarHubAlphabeticNoteNames.pdf ../GuitarHubAlphabeticNoteNames.pdf
 mv GuitarHubSolfegeNoteNames.pdf ../GuitarHubSolfegeNoteNames.pdf
 mv GuitarHubLyrics.pdf ../GuitarHubLyrics.pdf
+mv GuitarHubLyricsCompressedVersion.pdf ../GuitarHubLyricsCompressedVersion.pdf
 mv GuitarHubAlphabeticNoteNamesBooklet.pdf ../GuitarHubAlphabeticNoteNamesBooklet.pdf
 mv GuitarHubLyricsBooklet.pdf ../GuitarHubLyricsBooklet.pdf
+mv GuitarHubLyricsCompressedVersionBooklet.pdf ../GuitarHubLyricsCompressedVersionBooklet.pdf
 mv GuitarHubSolfegeNoteNamesBooklet.pdf ../GuitarHubSolfegeNoteNamesBooklet.pdf
 echo '
 --> pdf generated is moved to the main directory of GuitarHub
