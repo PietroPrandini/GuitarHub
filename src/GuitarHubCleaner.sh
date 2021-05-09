9ad777adfac902a206107fe54cf2df1e15f8ac54
Pietro Prandini <prandinip@gmail.com>
2021-05-09 16:08:21 +0200

    Cleaner

--- src/GuitarHubCleaner.sh
+++ src/GuitarHubCleaner.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
 
 # Copyright 2018-2021 Pietro Prandini
-# 
+#
 # This file is part of GuitarHub.
-# 
+#
 # GuitarHub is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # GuitarHub is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with GuitarHub.  If not, see <https://www.gnu.org/licenses/>.
 
-rm *.sbx
-rm *.sxd
-rm *.aux
-rm *.log
-rm *.out
-rm *.sxc
+PATHTOCLEAN="./ ./tex/main"
+
+isSuccess() {
+	exit_code=$1
+	if [ "${exit_code}" -eq "0" ];
+	then
+		echo "OK!"
+	else
+		echo "KO! Exit code: ${exit_code}"
+		exit 1
+	fi
+}
+
+for path in ${PATHTOCLEAN}
+do
+  for file in $(ls ${path} | grep ".aux\|.log\|.out\|.sbx\|.sxc\|.sxd")
+  do
+    echo "removing ${file}"
+    rm $file
+    isSuccess $?
+  done
+done
